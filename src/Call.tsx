c06942f1e4bd11b2c824269d03fb7a3f6657801e
ben <ben@wizschool.io>
2021-12-17 09:47:48 +0900

    Fix useEffect dependency

--- src/Call.tsx
+++ src/Call.tsx
@@ -23,7 +23,7 @@ function Call() {
         "00660dcb9c3c74b44b6b0b85aad121d4b3bIAA9ldSTy50tx/UtSSzwBGT56p1VMMbqgsdewW0UW5wbNatbzjAAAAAAEAA3+QZISS29YQEAAQBJLb1h",
       );
     }
-  }, [])
+  }, [isRecord, join])
 
   return (
     <div className='call'>
